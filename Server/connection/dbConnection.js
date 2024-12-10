1efb924da88150cae96a3d4fefe4582a9c492008
skcvlearn404 <skcvlearn404@gmail.com>
2024-12-10 14:14:30 +0545

    dev: initial setup

--- Server/connection/dbConnection.js
+++ Server/connection/dbConnection.js
@@ -4,11 +4,10 @@ require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORd,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
-    logging: false,
   }
 );
 module.exports = sequelize;
