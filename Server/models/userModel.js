d58ab1d1c1f5132cc17cbeaad7841900c0657697
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-21 22:24:52 +0545

    add confirm password field

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,33 +1,41 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../connection/dbConnection");
 
-const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const User = sequelize.define("User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    confirmPassword: {
+      type: DataTypes.VIRTUAL,
+      allowNull: true,
+      validate: {
+        isconfirmed(value) {
+          if (this.password && value != this.password) {
+            throw new Error("Password do not match.")
+          }
+        }
+      }
+    }
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = User;
