9740fbfd85f10a4d85a31108d9edd5a03aeb15e6
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-26 20:36:50 +0545

    run google oauth with passport

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const session = require("express-session");
 const sessionConfig = require("./config/sessionConfig.js");
+const configurePassport = require("./config/passportConfig.js");
 const userRoute = require("./routes/userRoutes");
+const googleAuthRoute = require("./routes/googleAuthRoutes.js");
 const expenseRoute = require("./routes/expenseRoutes");
 const incomeRoute = require("./routes/incomeRoutes");
 
@@ -9,11 +11,16 @@ const app = express();
 require("./models/index");
 
 app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
+
+configurePassport();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/v1/user", userRoute);
+app.use("/auth/google", googleAuthRoute);
 app.use("/v1/user-expense", expenseRoute);
 app.use("/v1/user-income", incomeRoute);
 
