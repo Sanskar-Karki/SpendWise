62586ce494ddbfe98b4c6f73783ce9bf73a8d5ed
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-28 10:46:24 +0545

    refactor JWT authmiddleware to support session and token

--- Server/middlewares/authMiddleware.js
+++ Server/middlewares/authMiddleware.js
@@ -1,18 +1,38 @@
 const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+const User = require("../models/userModel.js");
 
-const verifyJWT = (req, res, next) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
+const verifyJWT = async (req, res, next) => {
+  const jwtToken = req.cookiies["auth"];
+
+  const sessionToken = req.cookies["connect.sid"];
+
+  const token = sessionToken || jwtToken;
 
   if (!token) {
-    return res.status(403).json({ message: "No token proviede" });
+    return res.status(401).json({ message: "No token provided. Access denied." });
   }
 
-  jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "Unauthorized" });
+  try {
+    if (sessionToken) {
+      if (!req.user) {
+        return res.status(401).json({ message: "No user session found. Please login again." });
+      }
+      req.userId = req.user.id;
+      return next();
     }
-    req.userId = decoded.id;
-  });
-};
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_TOKEN_SECRET)
+    const user = await User.findOne({ where: { id: decoded.id } });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" })
+    }
+    req.userId = req.user.id;
+    return next();
+  } catch (error) {
+    console.error("Error while validating token: ", token);
+    return res.status(500).json({ message: "Error while token validation." })
+  }
+}
 
 module.exports = { verifyJWT };
